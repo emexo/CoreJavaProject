09808cd5ba866f43ab9f6241e2e7c50bdb7b0c82
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2023-11-01 06:56:36 +0530

    Add files via upload

---
+++ corejavaproject/src/main/java/com/emexo/variable/localvariable/Student.java
@@ -0,0 +1,9 @@
+package com.emexo.variable.localvariable;
+
+public class Student {
+    public static void main(String[] args) {
+        // local variable
+        int internalMark = 20;
+        System.out.println(internalMark);
+    }
+}
